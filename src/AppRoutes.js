9306ea46032e7f0b9c43893cbd2dd2cfa1c45283
herve risse <herve.risse91@gmail.com>
2023-09-06 21:09:35 +0800

    react router

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Accommodation from './pages/Accommodation';
+import Error from './pages/Error';
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/accommodation" element={<Accommodation />} />
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 };
